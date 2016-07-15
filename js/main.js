09bb86e5a09a6a43e0b75222d4ed737117741d09
Julio Aenlle <gmg8r251@ufl.edu>
2016-07-15 10:38:06 -0400

    Add event listener to js file

--- js/main.js
+++ js/main.js
@@ -7,3 +7,11 @@ var lnameInput = document.getElementById('lastName');
 var emailInput = document.getElementById('email');
 
 var messageInput = document.getElementById('message');
+
+document.addEventListener('submit', function (text) {
+    text.preventDefault();
+    console.log('The firstName is: ' + fnameInput.value);
+    console.log('The lastName is: ' + lnameInput.value);
+    console.log('The email is: ' + emailInput.value);
+    console.log('The message is: ' + messageInput.value);
+});
