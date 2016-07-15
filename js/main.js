936ef098c95cdc6b74daad71049bcc04be6da8b7
Julio Aenlle <gmg8r251@ufl.edu>
2016-07-15 10:34:56 -0400

    Add form variables to js file

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//Assisgnment Step 5
+
+var fnameInput = document.getElementById('firstName');
+
+var lnameInput = document.getElementById('lastName');
+
+var emailInput = document.getElementById('email');
+
+var messageInput = document.getElementById('message');
